c64e39c4d65f95fe4564114d2e8173695f0a031b
Andi Faizal <andifaizal647@gmail.com>
2022-03-26 14:57:40 +0700

    add migration:generate and migration:run

---
+++ src/migration/1648281400732-userMigration.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class userMigration1648281400732 implements MigrationInterface {
+    name = 'userMigration1648281400732'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "age" integer NOT NULL, "addres" character varying, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
